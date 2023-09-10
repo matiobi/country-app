168cf844164c30986421f618aeae26a78f7a6f09
Mateusz <mobirek@onet.eu>
2023-09-10 14:43:21 +0200

    HOMEPAGE WITH COUNTRY SIDE

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
 
 app = Flask(__name__)
 app.debug = True
+
+
+@app.route("/")
+def home_page():
+    try:
+        response = requests.get("https://restcountries.com/v3.1/all")
+        countries = response.json()
+        return render_template("index.html", countries = countries)
+    except Exception as e:
+        print("WYSTĄPIŁ BŁĄD", str(e))
+        return " NIE DZIAŁA "
+        
