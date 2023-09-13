965de1778ea4b0280e3cfece8e86211fa1dd43d9
Mateusz <mobirek@onet.eu>
2023-09-13 20:05:44 +0200

    FILERS_BY_COUNTRY

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
 
 app = Flask(__name__)
 app.debug = True
 
 
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home_page():
+    
+
+    search_query = request.form.get("query")
+    continent_filter = request.form.get("continent_filter")
+    print(search_query)
+
+    url =""
+    if search_query:
+        url = f"https://restcountries.com/v3.1/name/{search_query}"
+    else:
+        url = "https://restcountries.com/v3.1/all"
+
+
     try:
-        response = requests.get("https://restcountries.com/v3.1/all")
+        response = requests.get(url)
         countries = response.json()
+
+        if continent_filter:
+            countries = filter(lambda country:country['region'] == continent_filter, countries)
+
         return render_template("index.html", countries = countries)
     except Exception as e:
         print("WYSTĄPIŁ BŁĄD", str(e))
@@ -25,10 +42,17 @@ def country_page(name):
         native_name_key = list(data[0]['name']['nativeName'].keys())[0]
         languages = list(data[0]['languages'].values())
 
-        border_countries = ",".join(data[0]['borders'])
-        response_neighbours= requests.get(f"https://restcountries.com/v3.1/alpha?codes={border_countries}")
-        neighbours_data = list(response_neighbours.json())
-        neighbours = list(map(lambda neighbour: neighbour['name']['common'], neighbours_data))
+        neighbours = []
+
+        for field in data:
+          if "borders" not in field:
+              print("NIE MA BORDERS!!")
+          else:
+              border_countries = ",".join(data[0]['borders']) 
+              response_neighbours= requests.get(f"http://restcountries.com/v3.1/alpha?codes={border_countries}")
+              neighbours_data = list(response_neighbours.json())
+              neighbours = map(lambda neighbour: neighbour['name']['common'], neighbours_data)
+        
 
         country = {
           "currency_name": data[0]['currencies'][currency_key]['name'],
@@ -40,7 +64,7 @@ def country_page(name):
           "subregion" : data[0]['subregion'],
           "capital" : data[0]['capital'][0],
           "tld" : data[0]['tld'],
-          "coat_of_arms" : data[0]['coatOfArms']['svg']
+          "coat_of_arms":data[0]["coatOfArms"]['svg'] if data[0]['coatOfArms'] else data[0]['flags']['svg']
         }
 
         
